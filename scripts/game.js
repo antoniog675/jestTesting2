88d5161545b80b52f8662cb53054429f1e2a822b
antoniog675 <antoniogomez1@hotmail.co.uk>
2022-05-28 05:30:52 +0000

    Added more testing, added newGame functinon that also clear players turns and array and pushes new random values

--- scripts/game.js
+++ scripts/game.js
@@ -5,4 +5,21 @@ let game = {
     choices: ['button1', 'button2', 'button3', 'button4'],
 }
 
-module.exports = { game };
+function newGame() {
+    game.score = 0;
+    game.playerMoves = [];
+    game.currentGame = [];
+    showScore();
+    addTurn();
+}
+
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+}
+
+function showScore() {
+    document.getElementById('score').innerText = game.score;
+}
+
+module.exports = { game , newGame, showScore, addTurn};
